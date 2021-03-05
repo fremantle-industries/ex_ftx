25e6c8e95d11d27753ad2a670e4533b79638e2b2
Alex <alex+git@fremantle.io>
2021-03-05 14:05:52 -0800

    Release v0.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExFtx.MixProject do
   def project do
     [
       app: :ex_ftx,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.8",
       package: package(),
       start_permanent: Mix.env() == :prod,

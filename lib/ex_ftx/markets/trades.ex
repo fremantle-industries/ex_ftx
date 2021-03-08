786c7fa567e04158b69af0f0f765b657374f02a9
Alex <alex+git@fremantle.io>
2021-03-08 13:25:22 -0800

    Handle market not found in GET /markets/{market_name}/trades

--- lib/ex_ftx/markets/trades.ex
+++ lib/ex_ftx/markets/trades.ex
@@ -1,10 +1,15 @@
 defmodule ExFtx.Markets.Trades do
   @type market_name :: String.t()
   @type order_book :: ExFtx.Trade.t()
-  @type result :: {:ok, order_book} | {:error, :parse_result_item}
+  @type params :: %{
+          optional(:limit) => pos_integer,
+          optional(:start_time) => pos_integer,
+          optional(:end_time) => pos_integer
+        }
+  @type result :: {:ok, order_book} | {:error, :not_found | :parse_result_item | String.t()}
 
   @spec get(market_name) :: result
-  @spec get(market_name, map) :: result
+  @spec get(market_name, params) :: result
   def get(market_name, params \\ %{}) do
     "/markets/#{market_name}/trades"
     |> ExFtx.HTTPClient.non_auth_get(params)
@@ -21,5 +26,13 @@ defmodule ExFtx.Markets.Trades do
         _, _acc -> {:error, :parse_result_item}
       end
     )
+  end
+
+  defp parse_response({:ok, %ExFtx.JsonResponse{success: false, error: "No such market:" <> _}}) do
+    {:error, :not_found}
+  end
+
+  defp parse_response({:ok, %ExFtx.JsonResponse{success: false, error: reason}}) do
+    {:error, reason}
   end
 end
--- test/ex_ftx/markets/trades_test.exs
+++ test/ex_ftx/markets/trades_test.exs
@@ -8,11 +8,17 @@ defmodule ExFtx.Markets.TradesTest do
     :ok
   end
 
-  test ".get/1 with default depth" do
+  test ".get/1 with default limit, start & end time" do
     use_cassette "markets/trades/get_ok" do
       assert {:ok, trades} = ExFtx.Markets.Trades.get("BTC/USD")
-      assert Enum.count(trades) > 0
+      assert Enum.count(trades) == 20
       assert Enum.at(trades, 0).price > 0
+    end
+  end
+
+  test ".get/1 error market not found" do
+    use_cassette "markets/trades/get_error_market_not_found" do
+      assert {:error, :not_found} = ExFtx.Markets.Trades.get("INVALID/MARKET")
     end
   end
 end

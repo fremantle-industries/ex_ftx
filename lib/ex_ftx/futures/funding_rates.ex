5d64caff2baa3b45564607bbaf2ccb4d54200808
Alex <alex+git@fremantle.io>
2021-03-08 14:30:02 -0800

    Add params to GET /funding_rates

--- lib/ex_ftx/futures/funding_rates.ex
+++ lib/ex_ftx/futures/funding_rates.ex
@@ -1,12 +1,18 @@
 defmodule ExFtx.Futures.FundingRates do
   @type future_name :: String.t()
   @type funding_rate :: ExFtx.FundingRate.t()
-  @type result :: {:ok, [funding_rate]} | {:error, :parse_result_item}
+  @type params :: %{
+          optional(:future) => String.t(),
+          optional(:start_time) => non_neg_integer,
+          optional(:end_time) => non_neg_integer
+        }
+  @type result :: {:ok, [funding_rate]} | {:error, :not_found | :parse_result_item | String.t()}
 
   @spec get :: result
-  def get do
+  @spec get(params) :: result
+  def get(params \\ %{}) do
     "/funding_rates"
-    |> ExFtx.HTTPClient.non_auth_get(%{})
+    |> ExFtx.HTTPClient.non_auth_get(params)
     |> parse_response()
   end
 
@@ -20,5 +26,13 @@ defmodule ExFtx.Futures.FundingRates do
         _, _acc -> {:error, :parse_result_item}
       end
     )
+  end
+
+  defp parse_response({:ok, %ExFtx.JsonResponse{success: false, error: "No such future:" <> _}}) do
+    {:error, :not_found}
+  end
+
+  defp parse_response({:ok, %ExFtx.JsonResponse{success: false, error: reason}}) do
+    {:error, reason}
   end
 end
--- test/ex_ftx/futures/funding_rates_test.exs
+++ test/ex_ftx/futures/funding_rates_test.exs
@@ -8,12 +8,27 @@ defmodule ExFtx.Futures.FundingRatesTest do
     :ok
   end
 
-  test ".get/1" do
+  test ".get/0 all futures with default time period" do
     use_cassette "futures/funding_rates/get_ok" do
       assert {:ok, funding_rates} = ExFtx.Futures.FundingRates.get()
-      assert Enum.count(funding_rates) > 0
+      assert Enum.count(funding_rates) == 500
       assert %ExFtx.FundingRate{} = funding_rate = Enum.at(funding_rates, 0)
       assert funding_rate.future != nil
+    end
+  end
+
+  test ".get/1 single future with default time period" do
+    use_cassette "futures/funding_rates/get_single_future_ok" do
+      assert {:ok, funding_rates} = ExFtx.Futures.FundingRates.get(%{future: "BTC-PERP"})
+      assert Enum.count(funding_rates) == 500
+      assert %ExFtx.FundingRate{} = funding_rate = Enum.at(funding_rates, 0)
+      assert funding_rate.future != nil
+    end
+  end
+
+  test ".get/1 error future not found" do
+    use_cassette "futures/funding_rates/get_error_future_not_found" do
+      assert ExFtx.Futures.FundingRates.get(%{future: "INVALID-FUTURE"}) == {:error, :not_found}
     end
   end
 end
